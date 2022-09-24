cbf2747cc0aa65c838ff905d01d766fff1f9453b
Harit Meryem <haritmeryem@gmail.com>
2022-09-24 01:46:14 +0100

    ex00

---
+++ day00/ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char **argv)
+{
+	char	c;
+	int	i;
+	int	j;
+
+	i = 1;
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				c = toupper(argv[i][j]);
+				std::cout << c;
+				j++;
+			}
+			i++;
+		}
+	}
+	std::cout << "\n";
+	return (0);
+}
