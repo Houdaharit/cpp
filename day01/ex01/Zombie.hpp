ba50341ef8ee796d3c807dc516793a8b1eb235ce
Harit Meryem <haritmeryem@gmail.com>
2022-09-26 01:09:44 +0100

    N zombie

---
+++ day01/ex01/Zombie.hpp
@@ -0,0 +1,20 @@
+#ifndef ZOMBIE_H
+# define ZOMBIE_H
+# include <iostream>
+
+class	Zombie
+{
+	std::string	name;
+	public:
+	Zombie();
+	Zombie(std::string);
+	~Zombie();
+	void	announce(void)
+	{
+		std::cout << name << ": BraiiiiiiinnnzzzZ...\n";
+	}
+};
+
+Zombie	*newZombie(std::string);
+void	randomChump(std::string);
+#endif
