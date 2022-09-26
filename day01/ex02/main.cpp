0907b376795c4358049eb7bb006cef74d225e08e
Harit Meryem <houdaharit20@gmail.com>
2022-09-26 03:53:04 +0100

    maybe done

---
+++ day01/ex02/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+	std::string	str;
+	std::string	*stringPTR;
+	std::string	&stringREF = str;
+
+	str = "HI THIS IS BRAIN";
+	stringPTR = &str;
+	stringREF = str;
+
+	std::cout << "Address of str: " << &str << std::endl;
+	std::cout << "Address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Address held by stringREF: " << &stringREF << std::endl;
+	std::cout << std::endl;
+	std::cout << "Value of str: " << str << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+}
