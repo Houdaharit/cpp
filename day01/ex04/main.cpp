1869a52cbf8c5cd8b8469a8795f61bc9c9d3562e
Harit Meryem <houdaharit20@gmail.com>
2022-09-28 19:12:45 +0100

    halloween music

--- day01/ex04/main.cpp
+++ day01/ex04/main.cpp
@@ -2,5 +2,10 @@
 
 int main(int argc, char **argv)
 {
-
+	std::fstream	file;
+	if (argc < 4)
+		return (1);
+	file.open(argv[1], std::ios::in);
+	if (!file)
+		std::cerr << argv[1] << " can't be opened!" << std::endl;
 }
