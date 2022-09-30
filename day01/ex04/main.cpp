d7ae6d9fc0510b2c76d9a4c2d85f745c2901e758
Harit Meryem <houdaharit20@gmail.com>
2022-09-30 05:36:06 +0100

    done

--- day01/ex04/main.cpp
+++ day01/ex04/main.cpp
@@ -2,10 +2,21 @@
 
 int main(int argc, char **argv)
 {
-	std::fstream	file;
+	std::ifstream	file;
+	std::string	line;
 	if (argc < 4)
+	{
+		std::cerr << "Not enough parameters: " << 4 - argc << " parameters needed\n";
+		std::cerr << "File name\ts1\ts2\n";
 		return (1);
-	file.open(argv[1], std::ios::in);
+	}
+	file.open(argv[1]);
 	if (!file)
+	{
 		std::cerr << argv[1] << " can't be opened!" << std::endl;
+		return (1);
+	}
+	while (getline(file, line))
+		replace(line, argv[1], argv[2], argv[3]);
+	file.close();
 }
--- day01/ex04/replace.hpp
+++ day01/ex04/replace.hpp
@@ -2,5 +2,7 @@
 # define REPLACE_HPP
 # include <iostream>
 # include <fstream>
+# include <string>
 
+void	replace(std::string line, char*, char*, char*);
 # endif
