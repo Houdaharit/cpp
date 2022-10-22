5c5382a11a3d797b8f1de9fd18a6a4a61a349225
Houda Harit <houdaharit20@gmail.com>
2022-10-22 23:45:57 +0100

    not done yet

--- module01/ex04/replace.cpp
+++ module01/ex04/replace.cpp
@@ -5,13 +5,12 @@ void	replace(std::string line, char *file_name, char *s1, char *s2)
 	std::ofstream	new_file;
 	char		new_file_name[256];
 	size_t 		pos;
-	std::string	str;
 	std::string	str2;
 
-	(void)s1, (void)s2;
-	strncpy(new_file_name, file_name, sizeof(new_file_name - strlen(new_file_name) - 1));
-	strncat(new_file_name, ".replace", sizeof(new_file_name - strlen(new_file_name) - 1));
-	new_file.open(new_file_name, std::ios_base::app);
+	std::stringstream str(file_name);
+/*	strncpy(new_file_name, file_name, sizeof(new_file_name - strlen(new_file_name) - 1));
+	strncat(new_file_name, ".replace", sizeof(new_file_name - strlen(new_file_name) - 1));*/
+	new_file.open(str, std::ios_base::app);
 	if (!new_file)
 		std::cerr << "Hell no!\n";
 	pos = line.find(s1);
