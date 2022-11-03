827a29de1b3ced7b6aeb0abdc01b43fe91da6d7c
Houda Harit <houdaharit20@gmail.com>
2022-11-03 21:26:46 +0100

    done

--- module04/ex01/Animal.hpp
+++ module04/ex01/Animal.hpp
@@ -12,7 +12,7 @@ class Animal
 		Animal(const std::string&);
 		Animal(const Animal&);
 		Animal& operator = (const Animal&);
-		~Animal();
+		virtual ~Animal();
 		virtual void makeSound(void) const;
 		std::string getType(void) const;
 };
--- module04/ex01/main.cpp
+++ module04/ex01/main.cpp
@@ -3,8 +3,10 @@
 
 int main()
 {
-	Cat cat;
-	const Animal *animal = &cat;
-	animal->makeSound();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	delete j;//should not create a leak 
+	delete i;
+	while(1);
 	return 0;
 }
