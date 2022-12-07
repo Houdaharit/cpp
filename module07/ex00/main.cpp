45e84ea43154b53bbf3907daa0906f2c2a01c0ce
houda <houdaharit20@gmail.com>
2022-12-07 03:37:16 +0100

    done

--- module07/ex00/main.cpp
+++ module07/ex00/main.cpp
@@ -1,22 +1,19 @@
 #include "whatever.hpp"
 
-int main()
+int main( void )
 {
 	int a = 2;
 	int b = 3;
-
-	std::cout << "a= " << a << std::endl;
-	std::cout << "b= " << b << std::endl;
-	swap<int>(a, b);
-	std::cout << std::endl;
-	std::cout << "a= " << a << std::endl;
-	std::cout << "b= " << b << std::endl;
-	std::cout << "max: " << max<int>(a, b) << std::endl;
-	std::cout << "min: " << min<int>(a, b) << std::endl;
-	std::cout << std::endl;
-	std::cout << "max: " << max<float>(1.5f, 1.55f) << std::endl;
-	std::cout << "min: " << min<float>(1.5f, 1.55f) << std::endl;
-	std::cout << std::endl;
-	std::cout << "max: " << max<std::string>("hello", "Hello") << std::endl;
-	std::cout << "min: " << min<std::string>("hello", "Hello") << std::endl;
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
