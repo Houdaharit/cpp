100171e1ea471469161dfdf7814e9671a5e0fb09
Houda Harit <houdaharit20@gmail.com>
2022-12-16 22:23:13 +0100

    done

--- module07/ex00/whatever.hpp
+++ module07/ex00/whatever.hpp
@@ -12,21 +12,21 @@ void swap(T& a, T& b)
 }
 
 template <typename T>
-T min(T a, T b)
+T min(T& a, T& b)
 {
 	if (a < b)
-		return a;
+		return T(a);
 	else
-		return b;
+		return T(b);
 }
 
 template <typename T>
-T max(T a, T b)
+T max(T& a, T& b)
 {
 	if (b < a)
-		return a;
+		return T(a);
 	else
-		return b;
+		return T(b);
 }
 
 #endif
