825bdfc7a3d33de84b5a642ade9bfd235a0b6a83
Houda Harit <houdaharit20@gmail.com>
2022-12-17 20:27:22 +0100

    done

--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -5,13 +5,12 @@ int main()
 	//Integer
 	unsigned int n = 5;
 	Array<int> array(n);
-
-	for (int i = 0; i < array.size(); i++)
-		array.setValues(i, i);
-	for (int i = 0; i < array.size(); i++ )
-		std::cout << array[i] << std::endl;
 	try
 	{
+		for (int i = 0; i < array.size(); i++)
+			array.setValues(i, i);
+		for (int i = 0; i < array.size(); i++ )
+			std::cout << array[i] << std::endl;
 		std::cout << array[6] << std::endl;
 	}
 	catch(std::exception& e)
@@ -19,15 +18,14 @@ int main()
 		std::cout << e.what() << std::endl;
 	}
 	//string
-
-	Array<std::string> array2(n);
-	std::string str("hello");
-	for (int i = 0; i < array2.size(); i++)
-		array2.setValues(i, str);
-	for (int i = 0; i < array2.size(); i++ )
-		std::cout << array2[i] << std::endl;
 	try
 	{
+		Array<std::string> array2(n);
+		std::string str("hello");
+		for (int i = 0; i < array2.size(); i++)
+			array2.setValues(i, str);
+		for (int i = 0; i < array2.size(); i++ )
+			std::cout << array2[i] << std::endl;
 		std::cout << array2[6] << std::endl;
 	}
 	catch(std::exception& e)
