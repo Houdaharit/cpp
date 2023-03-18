b25df480f93ad9c20162233f2a84aff9b40ea206
houda <houdaharit20@gmail.com>
2023-03-18 03:26:09 +0100

    insert data && check it

--- module09/ex00/BitcoinExchange.cpp
+++ module09/ex00/BitcoinExchange.cpp
@@ -1,1 +1,25 @@
 #include <iostream>
+#include <map>
+#include <fstream>
+
+std::multimap insert_data(std::multimap& multimap)
+{
+
+}
+
+int main(int argc, char **argv)
+{
+	std::ifstream file;
+	std::string line;
+	std::multimap<std::string, int> multimap;
+
+	(void)argc;
+	file.open(argv[1]);
+	if (!file)
+	{
+		std::cerr << "Error: could not open file!" << std::endl;
+		return (1);
+	}
+	while(getline(file, line))
+		insert_data(multimap);
+}
