310927db966dcf5703f576c691790e4f523b36ec
Houda Harit <houdaharit20@gmail.com>
2023-03-25 21:33:20 +0000

    done

--- module09/ex02/PmergeMe.hpp
+++ module09/ex02/PmergeMe.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <deque>
+#include <sys/time.h>
 
 void mergesort_v(std::vector<int>& arr, std::vector<int>& temp, int l, int r, int threshold);
 void merge_v(std::vector<int>& arr, std::vector<int>& temp, int l, int m, int r);
@@ -10,5 +11,8 @@ void insertion_sort_v(std::vector<int>& vec, int l, int r);
 void mergesort_d(std::deque<int>& arr, std::deque<int>& temp, int l, int r, int threshold);
 void merge_d(std::deque<int>& arr, std::deque<int>& temp, int l, int m, int r);
 void insertion_sort_d(std::deque<int>& vec, int l, int r);
-
+void sort_vector(int argc, char **argv);
+void sort_deque(int argc, char **argv);
+void display_vector(std::vector<int>& v);
+void display_deque(std::deque<int>& q);
 #endif
--- module09/ex02/main.cpp
+++ module09/ex02/main.cpp
@@ -18,10 +18,17 @@ int	check_input(int argc, char **argv)
 void	insert_vector(std::vector<int> &v, int argc, char **argv)
 {
 	int i = 1;
+	size_t j = 0;
+	int input;
 
 	while (i < argc)
 	{
-		v.push_back(atoi(argv[i]));
+		j = 0;
+		input = atoi(argv[i]);
+		while(j < v.size() && v[j] != input)
+			j++;
+		if (j == v.size())
+			v.push_back(input);
 		i++;
 	}
 }
@@ -29,37 +36,83 @@ void	insert_vector(std::vector<int> &v, int argc, char **argv)
 void	insert_deque(std::deque<int> &q, int argc, char **argv)
 {
 	int i = 1;
+	int input;
+	size_t j;
 
 	while (i < argc)
 	{
-		q.push_back(atoi(argv[i]));
+		j = 0;
+		input = atoi(argv[i]);
+		while(j < q.size() && q[j] != input)
+			j++;
+		if (j == q.size())
+			q.push_back(input);
 		i++;
 	}
+}
+
+void	display_vector(std::vector<int>& v)
+{
+	if (v.size() > 20)
+	{
+		for(size_t i = 0; i < 20; i++)
+			std::cout << v[i] << " ";
+		std::cout << " [...]" << std::endl;
+	}
+	else
+	{
+		for(size_t i = 0; i < v.size(); i++)
+			std::cout << v[i] << " ";
+		std::cout << std::endl;
+	}
+}
+
+void	sort_vector(int argc, char **argv)
+{
+	std::vector<int> v;
+	struct timeval start;
+	struct timeval end_t;
+	size_t time;
+
+	insert_vector(v, argc, argv);
+	std::cout << "Before: "; 
+	display_vector(v);
+	std::vector<int> temp(v.size(), 0);
+	gettimeofday(&start, NULL);
+	mergesort_v(v, temp, 0, v.size() - 1, 2);
+	gettimeofday(&end_t, NULL);
+	time = ((end_t.tv_sec * 1000000) + end_t.tv_usec) - ((start.tv_sec * 1000000) + start.tv_usec);
+	std::cout << "After: "; 
+	display_vector(v);
+	std::cout << "Time to process a range of " << v.size() << " elements with std::vector: " << time << " us" << std::endl;
+}
+
+void	sort_deque(int argc, char **argv)
+{
+	std::deque<int> d;
+	struct timeval start;
+	struct timeval end_t;
+	size_t time;
+
+	insert_deque(d, argc, argv);
+	std::deque<int> temp(d.size(), 0);
+	gettimeofday(&start, NULL);
+	mergesort_d(d, temp, 0, d.size() - 1, 2);
+	gettimeofday(&end_t, NULL);
+	time = ((end_t.tv_sec * 1000000) + end_t.tv_usec) - ((start.tv_sec * 1000000) + start.tv_usec);
+	std::cout << "Time to process a range of " << d.size() << " elements with std::deque: " << time << " us" << std::endl;
 }
 
 int main(int argc, char **argv)
 {
 	std::vector<int> v;
-	std::deque<int> d;
 
 	if (argc > 1)
 	{
 		if (check_input(argc, argv) == -1)
 			return 1;
-		insert_vector(v, argc, argv);
-		insert_deque(d, argc, argv);
-		std::vector<int> temp_v(v.size(), 0);
-		std::deque<int> temp_d(d.size(), 0);
-		mergesort_v(v, temp_v, 0, v.size() - 1, 2);
-		mergesort_d(d, temp_d, 0, d.size() - 1, 2);
-		std::cout << "Vector: " << std::endl;
-		for(size_t i = 0; i < v.size(); i++)
-			std::cout << v[i] << " ";
-		std::cout << std::endl;
-		std::cout << "Deque: " << std::endl;
-		for(size_t i = 0; i < d.size(); i++)
-			std::cout << d[i] << " ";
-		std::cout << std::endl;
+		sort_vector(argc, argv);
+		sort_deque(argc, argv);
 	}
 	return 0;	
 }
