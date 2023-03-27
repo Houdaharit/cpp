a03e7a9d179c2322b52c607301738341cfa1add4
Houda Harit <houdaharit20@gmail.com>
2023-03-27 20:38:44 +0000

    k

--- module09/ex00/BitcoinExchange.cpp
+++ module09/ex00/BitcoinExchange.cpp
@@ -31,13 +31,13 @@ std::string strtrim(std::string& str)
 	return str;
 }
 
-std::multimap<std::string, std::string> insert_data(std::multimap<std::string, std::string>& multimap, std::string line)
+std::map<std::string, std::string> insert_data(std::map<std::string, std::string>& map, std::string line, const char* sep)
 {
 	int pos;
 	std::string date;
 	std::string value;
 
-	pos = line.find(",");
+	pos = line.find(sep);
 	date = line.substr(0, pos);
 	date = strtrim(date);
 	if (pos < 0)
@@ -47,46 +47,83 @@ std::multimap<std::string, std::string> insert_data(std::multimap<std::string, s
 		value = line.substr(pos + 1, line.size());
 		value = strtrim(value);
 	}
-	multimap.insert(std::pair<std::string, std::string>(date, value));
-	return multimap;
+	map.insert(std::pair<std::string, std::string>(date, value));
+	return map;
 }
 
-int main(int argc, char **argv)
+std::map<std::string, std::string> database_data(void)
 {
-	std::ifstream file;
 	std::ifstream database;
 	std::string line;
-	std::multimap<std::string, std::string> data;
-	std::multimap<std::string, std::string>::iterator it;
-
-	(void)argc, (void)argv;
-	/*file.open(argv[1]);
-	  if (!file)
-	  {
-	  std::cerr << "Error: could not open file!" << std::endl;
-	  return (1);
-	  }
-	  while(getline(file, line))
-	  insert_data(multimap, line);
-	  it = multimap.begin();
-	  while(it != multimap.end())
-	  {
-	  std::cout << it->first << " " << it->second << std::endl;
-	  ++it;
-	  }*/
+	std::map<std::string, std::string> data;
 
 	database.open("data.csv");
 	if (!database)
 	{
 		std::cerr << "Error: could not open file." << std::endl;
-		return 1;	
+		exit(1);
 	}
 	while(getline(database, line))
-		insert_data(data, line);
-	it = data.begin();
-	while(it != data.end())
+		insert_data(data, line, ",");
+	return (data);
+}
+
+void	date_value(std::string& line, td::string& date, int& value)
+{
+	int pos;
+
+	pos = line.find(sep);
+	date = line.substr(0, pos);
+	date = strtrim(date);
+	if (pos < 0)
+		value = "";
+	else
 	{
-		std::cout << it->first << " " << it->second << std::endl;
+		value = line.substr(pos + 1, line.size());
+		value = strtrim(value);
+	}
+}
+
+void	search_exchange_rate(int& date, std::string& date, std::map<std::string, std::string>& date)
+{
+	std::map<std::string,std::string>::iterator it;
+
+	it = date.begin();
+	while (it != data.end() && data.first != date)
 		++it;
+	if (it == data.end())
+	{
+		it = data.begin();
+		if (it->first == date)
+			std::cout << date << " => " << value << " = " << value * it->second << std::endl;
+		else
+		{
+			int min = INT_MAX;
+			while
+			if (abs(date - it->first) < min)
+				min = abs(date - it->first);
+		}
+	}
+}
+
+void	display(char *filename, std::map<std::string, std::string>& data)
+{
+	std::ifstream file;
+	std::string line;
+	std::string date;
+	int value;
+
+	line.open(filename);
+	if (!line)
+	{
+		std::cerr << "Error: could not open file." << std::endl;
+		exit(1);
+	}
+	while(getline(file, line))
+	{
+		date_value(line, date, value);
+		if (check_value_date(date, value) == -1)
+			continue;
+		search_exchange_rate(date, value, data);
 	}
 }
